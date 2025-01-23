3f27d1b64a2f5178480393dbc667464829fc96f5
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-01-23 15:22:30 -0600

    Removes unsed controller function

--- src/controllers/timeentry.controller.ts
+++ src/controllers/timeentry.controller.ts
@@ -10,21 +10,6 @@ class TimeEntryController {
       throw new Error("No jobs found.");
     }
   }
-
-  public async getJobById(id: string) {
-    if (ObjectId.isValid(id)) {
-      const result = await (
-        await timeEntriesCollection()
-      ).findOne({ _id: new ObjectId(id) });
-      if (result) {
-        return result;
-      } else {
-        throw new Error("Time entry not found.");
-      }
-    } else {
-      throw new Error("Invalid ID format.");
-    }
-  }
 }
 
 export default TimeEntryController;
