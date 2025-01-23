02a1a8cb1af2d00f5186385b2fe80f39fa911308
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-01-23 14:56:29 -0600

    Centers grid at large screens

--- src/components/TimeSheet.vue
+++ src/components/TimeSheet.vue
@@ -11,75 +11,75 @@ const timeframe = ref<"daily" | "weekly" | "monthly">("weekly");
 </script>
 
 <template>
-  <div
-    class="mx-auto grid gap-4 px-20 py-20 md:grid-cols-2 md:gap-7 md:px-32 lg:max-w-[1400px] lg:grid-cols-4 lg:px-36"
-  >
-    <section
-      class="min-w-64 rounded-2xl bg-secondaryDarkBlue md:col-span-2 md:row-span-1 md:min-w-0 lg:col-span-1 lg:row-span-2"
+  <div class="lg:flex lg:min-h-screen lg:items-center lg:justify-center">
+    <div
+      class="mx-auto grid gap-4 px-20 py-20 md:grid-cols-2 md:gap-7 md:px-32 lg:max-w-[1400px] lg:grid-cols-4 lg:px-36"
     >
-      <div
-        class="flex flex-row items-center gap-4 rounded-2xl bg-primaryBlue p-7 lg:flex-col lg:items-start lg:p-8 lg:pb-12"
+      <section
+        class="min-w-64 rounded-2xl bg-secondaryDarkBlue md:col-span-2 md:row-span-1 md:min-w-0 lg:col-span-1 lg:row-span-2"
       >
-        <img
-          src="image-jeremy.png"
-          alt="Avatar"
-          width="80"
-          class="rounded-full border-2 border-white lg:mb-9"
-        />
-        <div class="flex flex-col">
-          <h1>
-            <span class="text-base text-secondaryPaleBlue">Report for</span>
-          </h1>
-          <div>
-            <span
-              class="text-2xl font-light text-white lg:my-3 lg:inline-block lg:text-[2.5rem]"
-              >Jeremy</span
-            >
-            <span class="text-2xl font-light text-white lg:text-[2.5rem]">
-              Robson</span
-            >
+        <div
+          class="flex flex-row items-center gap-4 rounded-2xl bg-primaryBlue p-7 lg:flex-col lg:items-start lg:p-8 lg:pb-12"
+        >
+          <img
+            src="image-jeremy.png"
+            alt="Avatar"
+            width="80"
+            class="rounded-full border-2 border-white lg:mb-9"
+          />
+          <div class="flex flex-col">
+            <h1>
+              <span class="text-base text-secondaryPaleBlue">Report for</span>
+            </h1>
+            <div>
+              <span
+                class="text-2xl font-light text-white lg:my-3 lg:inline-block lg:text-[2.5rem]"
+                >Jeremy</span
+              >
+              <span class="text-2xl font-light text-white lg:text-[2.5rem]">
+                Robson</span
+              >
+            </div>
           </div>
         </div>
-      </div>
-
-      <nav>
-        <ul
-          class="flex flex-row justify-between gap-y-3 px-8 py-6 lg:flex-col lg:py-8"
-        >
-          <li
-            id="daily"
-            class="text-secondaryPaleBlue hover:cursor-pointer"
-            :class="{ '!text-white': timeframe === 'daily' }"
-            @click="timeframe = 'daily'"
+        <nav>
+          <ul
+            class="flex flex-row justify-between gap-y-3 px-8 py-6 lg:flex-col lg:py-8"
           >
-            Daily
-          </li>
-          <li
-            id="weekly"
-            class="text-secondaryPaleBlue hover:cursor-pointer"
-            :class="{ '!text-white': timeframe === 'weekly' }"
-            @click="timeframe = 'weekly'"
-          >
-            Weekly
-          </li>
-          <li
-            id="monthly"
-            class="text-secondaryPaleBlue hover:cursor-pointer"
-            :class="{ '!text-white': timeframe === 'monthly' }"
-            @click="timeframe = 'monthly'"
-          >
-            Monthly
-          </li>
-        </ul>
-      </nav>
-    </section>
-
-    <ActivityCard
-      client:load
-      :data="activity"
-      :timeframe="timeframe"
-      v-for="activity in activities"
-      :key="activity.title"
-    />
+            <li
+              id="daily"
+              class="text-secondaryPaleBlue hover:cursor-pointer"
+              :class="{ '!text-white': timeframe === 'daily' }"
+              @click="timeframe = 'daily'"
+            >
+              Daily
+            </li>
+            <li
+              id="weekly"
+              class="text-secondaryPaleBlue hover:cursor-pointer"
+              :class="{ '!text-white': timeframe === 'weekly' }"
+              @click="timeframe = 'weekly'"
+            >
+              Weekly
+            </li>
+            <li
+              id="monthly"
+              class="text-secondaryPaleBlue hover:cursor-pointer"
+              :class="{ '!text-white': timeframe === 'monthly' }"
+              @click="timeframe = 'monthly'"
+            >
+              Monthly
+            </li>
+          </ul>
+        </nav>
+      </section>
+      <ActivityCard
+        client:load
+        :data="activity"
+        :timeframe="timeframe"
+        v-for="activity in activities"
+        :key="activity.title"
+      />
+    </div>
   </div>
 </template>
