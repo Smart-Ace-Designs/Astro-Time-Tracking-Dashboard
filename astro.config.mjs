cd680c842ab4bf9ade4b4c9260e16d770688d1e4
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-10-28 15:41:38 -0500

    Converts project to use experimental Astro fonts; fixes incorrect font (Roboto instead of Rubik)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -15,4 +15,13 @@ export default defineConfig({
   integrations: [vue()],
   output: "server",
   adapter: netlify(),
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Rubik",
+        cssVariable: "--font-rubik",
+      },
+    ],
+  },
 });
